7b4f49e5caeb5034a8a69193b725a91c4422676a
Emann-script <olajubajeifeoluwa93@gmail.com>
2025-07-08 19:47:18 +0100

    adjustment to hero component

--- src/components/Home/Hero.vue
+++ src/components/Home/Hero.vue
@@ -1,35 +1,30 @@
 <template>
-    <section class="relative w-ful min-h-full overflow-hidden pb-56">
-        <img src="../../assets/jcf/jasmine_1_1920x1080.jpg" alt="Hero Background"
-            class="fixed top-0 left-0 w-full h-full object-cover z-0" style="min-height: 100%; height: 100%;" />
-        <div class="relative z-1">
-            <div class="grid grid-col-2 gap-5 items-center pt-32 text-white px-1 max-w-6xl mx-auto">
-                <Transition name="fade" mode="in-out" duration="300">
-                    <div class="col-span-1 leading-0 mt-24">
-                        <h1 class="text-5xl font-bold leading-normal scale-y-90" id="hero-header">EFFORTLESSLY BLEND<br>
-                            COMFORT &
-                            STYLE!</h1>
-                    </div>
-                </Transition>
-                <div class="col-span-1 flex items-center">
-                    <router-link to="/everything"
-                        class="bg-[#fceef5] text-[#0f172a] py-3 px-8 text-sm rounded-none shadow-lg font-semibold hover:bg-[#0f172a] hover:text-white">
-                        SHOP NOW
-                    </router-link>
-                </div>
+    <section class="relative w-full h-screen -mt-15 bg-fixed bg-center bg-cover bg-no-repeat"
+         :style="`background-image: url('${heroBg}')`">
+        <div class="absolute inset-0 bg-gradient-to-tr from-[#140610]/50 to-[#0084D6]/40"></div>
+        <div class="relative z-20 max-w-6xl mx-auto h-full grid grid-col-2 items-center text-white pt-43">
+            <div class="col-span-1">
+                <h1 class="text-6xl font-bold leading-normal scale-y-80">
+                    EFFORTLESSLY BLEND<br />
+                    COMFORT & STYLE!
+                </h1>
+            </div>
+            <div class="col-span-1 flex items-center -mt-80">
+                <router-link to="/everything"
+                    class="bg-[#fceef5] text-[#0f172a] py-3 px-8 text-sm rounded-none shadow-lg font-semibold hover:bg-[#0f172a] hover:text-white">
+                    SHOP NOW
+                </router-link>
             </div>
         </div>
-        <div
-            class="absolute top-0 left-0 w-full h-full inset-0 bg-gradient-to-tr from-[#140610]/50 to-[#0084D6]/40  bg-transparent pointer-events-none">
-
-        </div>
     </section>
+
 
 
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router'
+import heroBg from '@/assets/jcf/jasmine_1_1920x1080.jpg'
 
 const route = useRoute()
 
